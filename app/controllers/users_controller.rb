8340838a41ba13e7c668387e199ec09eeeb64421
Jason Lutz <jason.m.lutz@gmail.com>
2020-08-04 14:26:02 -0700

    Create users controller skeleton, routes, tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def new
+    render :new
+  end
+
+  def show
+
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  describe "GET #new" do
+      it "renders the new template" do
+        get :new, {}
+        expect(response).to render_template("new")
+        expect(response).to have_http_status(200)
+      end
+    end
 
-end
+    describe "POST #create" do
+      context "with invalid params" do
+        it "validates the presence of the user's email" do
+          post :create, params: { user: { email: 'jason@fakesite.org' } }
+          expect(response).to render_template('new')
+          expect(flash[:errors]).to be_present
+        end
+
+        it "validates the presence of the user's password" do
+          post :create, params: { user: { password: 'good_password' } }
+          expect(response).to render_template('new')
+          expect(flash[:errors]).to be_present
+        end
+
+        it "validates that the password is at least 6 characters long" do
+          post :create, params: { user: { email: 'jason@fakesite.org', password: 'abc' } }
+          expect(response).to render_template('new')
+          expect(flash[:errors]).to be_present
+        end
+      end
+
+      context "with valid params" do
+        it "redirects user to bands index on success" do
+          post :create, params: { user: { email: 'jason@fakesite.com', password: 'good_password' } }
+          expect(response).to redirect_to(user_url(User.last))
+        end
+      end
+    end
+  end
