8c4194a8f5e22a843c2a0b7a0b54152fabfa8ee6
Jason Lutz <jason.m.lutz@gmail.com>
2020-08-13 19:25:00 -0700

    Include new user specs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to goals_url
+      redirect_to user_url(@user)
     else
       flash[:errors] = @user.errors.full_messages
       render :new
--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -1,1 +1,27 @@
 require 'rails_helper'
+
+feature 'the signup process' do
+  scenario 'has a new user page' do
+    visit new_user_url
+    expect(page).to have_content 'Register'
+  end
+
+  feature 'signing up a user' do
+    before(:each) do
+      visit new_user_url
+      fill_in 'Email', with: 'testing_username'
+      fill_in 'Password', with: 'biscuits'
+      click_on 'Register'
+    end
+
+    scenario 'redirects to user page after signup' do
+      expect(page).to have_content 'User page!'
+    end
+
+    scenario 'shows username on the user\'s page after signup' do
+      expect(page).to have_content 'testing_username'
+    end
+  end
+
+  
+end
